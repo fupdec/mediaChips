9475da2abd804c565313fe00d1f508e6ab29b810
fupdec <>
2021-05-14 13:00:24 +0300

    a. meta in cards

--- src/components/pages/meta/MetaCard.vue
+++ src/components/pages/meta/MetaCard.vue
@@ -5,6 +5,9 @@
       v-ripple="{ class: 'accent--text' }">
       <v-icon>mdi-card</v-icon>
       <div>{{metaCard.meta.name}}</div>
+      <div v-for="(m,i) in metaInCard" :key="i">
+        <v-icon>mdi-{{getMeta(m.id,m.type).settings.icon}}</v-icon>
+      </div>
     </v-card>
   </v-lazy>
 </template>
@@ -26,24 +29,21 @@ export default {
     cardKey: '',
   }),
   computed: {
-    metaId() {
-      return this.$route.query.metaId
-    },
-    meta() {
-      return this.$store.getters.meta.find({id: this.metaId}).value()
-    },
-    cardSize() {
-      return `calc(${100 / this.meta.settings.cardSize}% - 20px)`
-    },
-    updateCardIds() {
-      return this.$store.state.Meta.updateCardIds
-    },
+    metaId() { return this.$route.query.metaId },
+    meta() { return this.$store.getters.meta.find({id: this.metaId}).value() },
+    cardSize() { return `calc(${100 / this.meta.settings.cardSize}% - 20px)` },
+    updateCardIds() { return this.$store.state.Meta.updateCardIds },
+    metaInCard() { return this.meta.settings.metaInCard },
   },
   methods: {
     stopSmoothScroll(event) {
       if (event.button != 1) return
       event.preventDefault()
       event.stopPropagation()
+    },
+    getMeta(id, type) {
+      if (type == 'complex') return this.$store.getters.meta.find({id}).value()
+      else return this.$store.getters.simpleMeta.find({id}).value()
     },
   },
   watch: {
