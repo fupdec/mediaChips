123a96d17b3b2f1773bc99d2c22bc09c10f23d00
fupdec <>
2021-06-20 14:23:19 +0300

    c. backups management

--- src/components/pages/settings/ManageBackups.vue
+++ src/components/pages/settings/ManageBackups.vue
@@ -1,45 +1,41 @@
 <template>   
   <div>
-    <v-row>
-      <v-col cols="10">
-        <v-data-table
-          v-model="selectedBackup" :headers="headers" :items="backups" item-key="date"
-          single-select show-select dense class="elevation-2"
-          no-data-text="No backups have been created" @input="backupError=false"
-        ><template v-slot:top>
-          <v-alert type="error" dense text :value="backupError">
-            Please select a backup
-          </v-alert>
-        </template>
-        </v-data-table>
-      </v-col>
-      <v-col cols="2">
-        <v-btn @click="dialogCreateBackup = true" 
-          light depressed small block class="backup-btn" color="#8ffe71"
-        ><v-icon left>mdi-database-plus</v-icon> Create
-        </v-btn>
+    <v-btn @click="openDialog" color="primary" block x-large rounded>
+      <v-icon large class="mr-4">mdi-database</v-icon> Manage backups </v-btn>
 
-        <v-btn  @click="openDialogRestoreBackup" :disabled="isNoBackups"
-          light depressed small block class="backup-btn" color="#ffdf97" 
-        ><v-icon left>mdi-database-refresh</v-icon> Restore 
-        </v-btn>
-
-        <v-btn  @click="importBackup"
-          light depressed small block class="backup-btn" color="#91cdff" 
-        ><v-icon left>mdi-database-import</v-icon> Import 
-        </v-btn>
-        
-        <v-btn @click="exportBackup" :disabled="isNoBackups"
-          light depressed small block class="backup-btn" color="#ffbcd8"
-        ><v-icon left>mdi-database-export</v-icon> Export 
-        </v-btn>
-
-        <v-btn @click="openDialogDeleteBackup" :disabled="isNoBackups"
-          light depressed small block class="backup-btn" color="#ff6f64"
-        ><v-icon left>mdi-database-remove</v-icon> Delete 
-        </v-btn>
-      </v-col>
-    </v-row>
+    <v-dialog v-if="dialog" v-model="dialog" scrollable persistent max-width="800">
+      <v-card>
+        <v-toolbar color="primary">
+          <span class="headline">Backups management</span>
+          <v-spacer></v-spacer>
+          <v-btn @click="dialog=false, backups=[]" outlined> <v-icon left>mdi-close</v-icon> Close </v-btn>
+        </v-toolbar>
+        <vuescroll>
+          <v-card-text>
+            <v-alert :value="backupError" type="info" dense text> Please select a backup </v-alert>
+            <v-data-table v-model="selectedBackup" :headers="headers" :items="backups" item-key="date"
+              single-select show-select dense class="elevation-2"
+              no-data-text="No backups have been created" @input="backupError=false"/>
+          </v-card-text>
+        </vuescroll>
+        <v-card-actions class="mb-2">
+          <v-btn @click="dialogCreateBackup=true" class="pr-4" rounded color="success">
+            <v-icon left>mdi-database-plus</v-icon> Create </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn  @click="openDialogRestoreBackup" :disabled="isNoBackups" class="pr-4" rounded color="warning">
+            <v-icon left>mdi-database-refresh</v-icon> Restore </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="openDialogDeleteBackup" :disabled="isNoBackups" class="pr-4" rounded color="error">
+            <v-icon left>mdi-database-remove</v-icon> Delete </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn  @click="importBackup" class="pr-4" rounded color="info">
+            <v-icon left>mdi-database-import</v-icon> Import </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn @click="exportBackup" :disabled="isNoBackups" class="pr-4" rounded color="info">
+            <v-icon left>mdi-database-export</v-icon> Export </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-dialog v-model="dialogCreateBackup" width="600" persistent>
       <v-card>
         <v-toolbar color="success">
@@ -109,14 +105,13 @@ const { ipcRenderer } = require('electron')
 const archiver = require('archiver')
 const StreamZip = require('node-stream-zip')
 
+import vuescroll from 'vuescroll'
+
 export default {
   name: 'ManageBackups',
-  components: {
-	},
+  components: { vuescroll },
   mounted() {
     this.$nextTick(function () {
-      if (this.$store.state.Settings.backups.length!==0) this.isNoBackups=false
-      this.backups = this.getBackups()
     })
   },
   data: () => ({
@@ -127,37 +122,48 @@ export default {
     headers: [
       { text: 'Date, time', value: 'date' },
       { text: 'Videos', value: 'videos' },
-      { text: 'Performers', value: 'performers' },
-      { text: 'Tags', value: 'tags' },
-      { text: 'Websites', value: 'websites' },
       { text: 'Total size (MB)', value: 'size', sort: (a,b)=>(a-b) }, 
+      { text: 'Version', value: 'version' }, 
     ],
     backupError: false,
-    isNoBackups: true,
     isBackupRestoredSuccessfully: false,
     isBackupRestoredError: false,
+    dialog: false,
     dialogCreateBackup: false,
     dialogRestoreBackup: false,
     dialogDeleteBackup: false,
+    compatibleVersions: ['0.9.0'],
   }),
   computed: {
-    pathToUserData() {
-      return this.$store.getters.getPathToUserData
-    },
+    pathToUserData() { return this.$store.getters.getPathToUserData },
+    isNoBackups() { return this.backups.length == 0 },
   },
   methods: {
     getBackups() {
-      if (this.$store.state.Settings.backups.length === 0) {
-        this.isNoBackups = true 
-      } else {
-        this.isNoBackups = false
-      }
-      let backups = JSON.parse(JSON.stringify(this.$store.state.Settings.backups))
-      let backupsFixedSize = backups.map(backup=>{
-        backup.size = (backup.size / 1024 / 1024).toFixed(2)
-        return backup
+      let backups = []
+      const backupsPath = path.join(this.pathToUserData, '/backups/')
+      fs.readdirSync(backupsPath).forEach(file => {
+        if (path.extname(file) !== '.zip') return false
+        const pathToFile = path.join(backupsPath, file)
+        const zip = new StreamZip({ file: pathToFile, storeEntries: true })
+        zip.on('error', err => { 
+          this.$store.commit('addLog', {
+            type: 'error',
+            text: `Backup ${file} corrupted. Please remove it manually.`
+          })
+          zip.close()
+          return false
+        })
+        zip.on('ready', () => {
+          let backupInfo = zip.entryDataSync('info.json').toString('utf8')
+          backupInfo = JSON.parse(backupInfo)
+          if (backupInfo.version===undefined) backupInfo.version = '0.8.2'
+          backupInfo.size = (fs.statSync(pathToFile).size / 1024 / 1024).toFixed(2)
+          backups.push(backupInfo)
+          zip.close()
+        })
       })
-      return backupsFixedSize
+      return backups
     },
     clearFiles(directory) {
       return new Promise((resolve, reject) => {
@@ -165,30 +171,16 @@ export default {
           console.log(directory,files)
           if (err) return reject(err)
           if (files.length == 0) return resolve()
-          
           async function unlinkFiles(files) {
             for (const file of files) {
               const filePath = path.join(directory, file)
-              try {
-                fs.promises.unlink(filePath)
-              } catch (error) {
-                return reject(err)
-              }
+              try { fs.promises.unlink(filePath) } 
+              catch (error) { return reject(err) }
             }
           }
-
-          unlinkFiles(files).then(() => {
-            return resolve()
-          })
+          unlinkFiles(files).then(() => { return resolve() })
         })
       })
-    },
-    getRestoreBackupRules(name) {
-      if (name.length===0) {
-        return 'Need to choose any backup'
-      } else {
-        return true
-      }
     },
     createBackup() {
       this.isCreatingBackupRun = true
@@ -212,23 +204,19 @@ export default {
           
       backupInfo.date = backupName
       backupInfo.videos = this.$store.getters.videosTotal
-      backupInfo.performers = this.$store.getters.performersTotal
-      backupInfo.tags = this.$store.getters.tagsTotal
-      backupInfo.websites = this.$store.getters.websitesTotal
+      backupInfo.version = this.$store.state.Settings.databaseVersion || '0.8.2'
 
-      console.log('backup creation started')
+      // console.log('backup creation started')
       let output = fs.createWriteStream(path.join(this.pathToUserData,'/backups/'+backupName+'.zip'))
       let archive = archiver('zip')
       output.on('close', function() {
         backupInfo.size = archive.pointer()
-        vm.$store.getters.settings.get('backups').push(backupInfo).write()
         vm.$store.dispatch('setNotification', {type:'success', text:'Backup successfully created'})
         vm.$store.commit('addLog', {text:'💾 Backup successfully created', type:'success'})
         vm.isCreatingBackupRun = false
-        vm.$store.dispatch('updateBackups')
         vm.backups = vm.getBackups()
-        console.log(archive.pointer() + ' total bytes')
-        console.log('archiver has been finalized and the output file descriptor has closed.')
+        // console.log(archive.pointer() + ' total bytes')
+        // console.log('archiver has been finalized and the output file descriptor has closed.')
         vm.dialogCreateBackup = false
       })
       archive.on('error', function(err) { 
@@ -246,6 +234,13 @@ export default {
       if (this.selectedBackup.length == 0) {
         this.backupError = true
         return false
+      } else if (!this.compatibleVersions.includes(this.selectedBackup[0].version)) {
+        this.$store.dispatch('setNotification', {
+          type: 'error',
+          text: 'This backup is not compatible with the current version of the application'
+        })
+        this.backupError = false
+        return false
       } else {
         this.dialogRestoreBackup = true
         this.backupError = false
@@ -258,9 +253,6 @@ export default {
       // clear folders with media
       await this.clearFiles(path.join(appFiles, 'thumbs/'))
       await this.clearFiles(path.join(appFiles, 'previews/'))
-      await this.clearFiles(path.join(appFiles, 'performers/'))
-      await this.clearFiles(path.join(appFiles, 'tags/'))
-      await this.clearFiles(path.join(appFiles, 'websites/'))
       await this.clearFiles(path.join(appFiles, 'markers/'))
       await this.clearFiles(path.join(this.pathToUserData, '/databases/'))
       
@@ -305,84 +297,66 @@ export default {
       })
     },
     importBackup() {
-      dialog.showOpenDialog(null, {
-        properties: ['openFile']
-      }).then(result => {
-        if (result.filePaths.length !== 0) {
-          let backupPath = result.filePaths[0]
-          let extension = backupPath.substr(backupPath.lastIndexOf('.') + 1)
-          if (extension !== 'zip') {
-            this.$store.dispatch('setNotification', {
-              type: 'error',
-              text: 'The selected file is not a backup'
-            })
-            return false
-          }
-          let backupInfo = {}
-          // read info.json from backup archive
-          const zip = new StreamZip({
-            file: backupPath,
-            storeEntries: true
-          })
-          zip.on('ready', () => {
-            backupInfo = zip.entryDataSync('info.json').toString('utf8')
-            backupInfo = JSON.parse(backupInfo)
+      let importPath = path.join(this.pathToUserData, '/backups/')
 
-            let importPath = path.join(this.pathToUserData, '/backups/')
-            let duplicate = this.$store.getters.settings.get('backups').find({'date':backupInfo.date}).value()
-            if (duplicate !== undefined) {
-              this.$store.dispatch('setNotification', {
-                type: 'error',
-                text: 'This backup already exists'
-              })
-              return false
-            } else {
-              backupInfo.size = fs.statSync(backupPath).size
-              fs.copyFile(backupPath, importPath+backupInfo.date+'.zip', (err) => {
-                if (err) {
-                  this.$store.commit('addLog', { text: err, type: 'error' })
-                  throw err
-                } else {
-                  this.$store.getters.settings.get('backups').push(backupInfo).write()
-                  this.$store.commit('addLog', { 
-                    text: `Backup "${backupInfo.date}" successfully imported`, 
-                    type: 'success'
-                  })
-                  // TODO recreate backup info for new meta system. 
-                  // no more tags, webs, perfs in backup info
-                  this.$store.dispatch('updateBackups')
-                  this.backups = this.getBackups()
-                }
-              })
-            }
-            zip.close()
+      dialog.showOpenDialog(null, { properties: ['openFile'] }).then(result => {
+        if (result.filePaths.length === 0) return false
+        let backupPath = result.filePaths[0]
+        if (path.extname(backupPath) !== '.zip') {
+          this.$store.dispatch('setNotification', {
+            type: 'error',
+            text: 'The selected file is not a backup'
           })
+          return false
+        } else if (fs.existsSync(path.join(importPath, path.basename(backupPath)))) {
+          this.$store.dispatch('setNotification', {
+            type: 'error',
+            text: 'This backup already exists'
+          })
+          return false
         }
-      }).catch(err => { this.$store.commit('addLog', { text: err, type: 'error' }) })
-    },
-    exportBackup() {
-      if (this.selectedBackup.length == 0) {
-        this.backupError = true
-        return false
-      } else {
-        this.backupError = false
-      }
-      dialog.showOpenDialog(null, {
-        properties: ['openDirectory']
-      }).then(result => {
-        if (result.filePaths.length !== 0) {
-          let date = this.selectedBackup[0].date
-          let backupPath = path.join(this.pathToUserData, '/backups/'+date+'.zip')
-          let exportPath = path.join(result.filePaths[0], '/'+date+'.zip')
-          fs.copyFile(backupPath, exportPath, (err) => {
+        // read info.json from backup archive
+        const zip = new StreamZip({ file: backupPath, storeEntries: true })
+        zip.on('error', err => { 
+          this.$store.dispatch('setNotification', {
+            type: 'error',
+            text: 'The selected file is not a backup'
+          })
+          zip.close()
+          return false
+        })
+        zip.on('ready', () => {
+          let backupInfo = zip.entryDataSync('info.json').toString('utf8')
+          backupInfo = JSON.parse(backupInfo)
+
+          fs.copyFile(backupPath, importPath+backupInfo.date+'.zip', (err) => {
             if (err) {
               this.$store.commit('addLog', { text: err, type: 'error' })
               throw err
             } else {
-              this.$store.commit('addLog', { text: `Backup "${date}" successfully exported`, type: 'success' })
+              this.$store.commit('addLog', { 
+                text: `Backup "${backupInfo.date}" successfully imported`, 
+                type: 'success'
+              })
+              this.backups = this.getBackups()
             }
           })
-        }
+          zip.close()
+        })
+      }).catch(err => { this.$store.commit('addLog', { text: err, type: 'error' }) })
+    },
+    exportBackup() {
+      if (this.selectedBackup.length == 0) { this.backupError = true; return false } 
+      else this.backupError = false
+      dialog.showOpenDialog(null, { properties: ['openDirectory'] }).then(result => {
+        if (result.filePaths.length === 0) return false
+        let date = this.selectedBackup[0].date
+        let backupPath = path.join(this.pathToUserData, '/backups/'+date+'.zip')
+        let exportPath = path.join(result.filePaths[0], '/'+date+'.zip')
+        fs.copyFile(backupPath, exportPath, (err) => {
+          if (err) { this.$store.commit('addLog', { text: err, type: 'error' }) ; throw err } 
+          else this.$store.commit('addLog', { text: `Backup "${date}" successfully exported`, type: 'success' })
+        })
       }).catch(err => { this.$store.commit('addLog', { text: err, type: 'error' }) })
     },
     openDialogDeleteBackup() {
@@ -399,26 +373,20 @@ export default {
       let backupPath = path.join(this.pathToUserData, '/backups/'+backupDate+'.zip')
       if (fs.existsSync(backupPath)) {
         fs.unlink(backupPath, err => {
-          if (err) {
-            this.$store.commit('addLog', { text: err, type: 'error' })
-            throw err
-          } else {
-            this.deleteBackupFromDb(backupDate)
-          }
+          if (err) { this.$store.commit('addLog', { text: err, type: 'error' }); throw err }
+          else this.deleteBackupFromDb(backupDate)
         })
-      } else {
-        this.deleteBackupFromDb(backupDate)
-      }
+      } else this.deleteBackupFromDb(backupDate)
       this.dialogDeleteBackup = false
     },
     deleteBackupFromDb(backupDate) {
-      this.$store.getters.settings.get('backups').remove({ 'date': backupDate }).write()
       this.$store.commit('addLog', { text: `Backup "${backupDate}" successfully deleted`, type: 'info' })
-      this.$store.dispatch('updateBackups')
       this.backups = this.getBackups()
     },
-    restartApp() {
-      ipcRenderer.send('reload')
+    restartApp() { ipcRenderer.send('reload') },
+    openDialog() {
+      this.dialog = true
+      setTimeout(() => { this.backups = this.getBackups() }, 500)
     },
   },
 }
