2eac258362aca55259360442018a80620f919afe
fupdec <>
2021-09-15 23:26:41 +0300

    f. overflow for context menu

--- src/components/app/ContextMenu.vue
+++ src/components/app/ContextMenu.vue
@@ -2,28 +2,33 @@
   <v-menu v-model="$store.state.contextMenu" :position-x="$store.state.x" :position-y="$store.state.y"
     leave-absolute absolute offset-y z-index="1000" min-width="150" :close-on-content-click="false">
     <v-list dense class="context-menu">
-      <div v-for="(item, i) in content" :key="i">
-        <v-list-item v-if="item.type=='item'" @mouseup="close(item.function)" :disabled="item.disabled" class="pr-1" link>
-          <v-list-item-title> 
-            <v-icon left size="18" :color="item.color">mdi-{{item.icon}}</v-icon> {{item.name}} 
-          </v-list-item-title>
-          <div class="px-3"/>
-        </v-list-item>
+      <vuescroll>
+        <div class="wrapper">
+          <div v-for="(item, i) in content" :key="i">
+            <v-list-item v-if="item.type=='item'" @mouseup="close(item.function)" :disabled="item.disabled" class="pr-1" link>
+              <v-list-item-title> 
+                <v-icon left size="18" :color="item.color">mdi-{{item.icon}}</v-icon> {{item.name}} 
+              </v-list-item-title>
+              <div class="px-3"/>
+            </v-list-item>
 
-        <v-divider v-else-if="item.type=='divider'" class="ma-1"></v-divider>
-        
-        <RecursiveMenu v-else-if="item.type=='menu'" :item="item" />
-      </div>
+            <v-divider v-else-if="item.type=='divider'" class="ma-1"></v-divider>
+            
+            <RecursiveMenu v-else-if="item.type=='menu'" :item="item" />
+          </div>
+        </div>
+      </vuescroll>
     </v-list>
   </v-menu>
 </template>
 
 <script>
+import vuescroll from 'vuescroll'
 import RecursiveMenu from '@/components/elements/RecursiveMenu.vue'
 
 export default {
   name: 'ContextMenu',
-  components: { RecursiveMenu },
+  components: { RecursiveMenu, vuescroll },
   computed: {
     content() { return this.$store.state.contextMenuContent },
   },
--- src/components/elements/RecursiveMenu.vue
+++ src/components/elements/RecursiveMenu.vue
@@ -3,30 +3,35 @@
     <template v-slot:activator="{ on, attrs }">
       <v-list-item class="pr-1" link v-bind="attrs" v-on="on" :disabled="item.disabled">
         <v-list-item-title> 
-          <v-icon left size="18">mdi-{{item.icon}}</v-icon> {{item.name}}
+          <v-icon left size="18" :color="item.color">mdi-{{item.icon}}</v-icon> {{item.name}}
         </v-list-item-title>
         <v-icon size="22">mdi-menu-right</v-icon>
       </v-list-item>
     </template>
-    
+
     <v-list dense class="context-menu">
-      <div v-for="(itemSub, j) in item.menu" :key="j">
-        <v-list-item v-if="itemSub.type=='item'" @mouseup="close(itemSub.function)" class="pr-1" link>
-          <v-list-item-title>
-            <v-icon left size="18" :color="itemSub.color">mdi-{{itemSub.icon}}</v-icon> {{itemSub.name}} 
-          </v-list-item-title>
-          <div class="px-3"/>
-        </v-list-item>
+      <vuescroll>
+        <div class="wrapper">
+          <div v-for="(itemSub, j) in item.menu" :key="j">
+            <v-list-item v-if="itemSub.type=='item'" @mouseup="close(itemSub.function)" class="pr-1" link>
+              <v-list-item-title>
+                <v-icon left size="18" :color="itemSub.color">mdi-{{itemSub.icon}}</v-icon> {{itemSub.name}} 
+              </v-list-item-title>
+              <div class="px-3"/>
+            </v-list-item>
 
-        <v-divider v-else-if="itemSub.type=='divider'" class="ma-1"></v-divider>
+            <v-divider v-else-if="itemSub.type=='divider'" class="ma-1"></v-divider>
 
-        <RecursiveMenu v-else-if="itemSub.type=='menu'" :item="itemSub" />
-      </div>
+            <RecursiveMenu v-else-if="itemSub.type=='menu'" :item="itemSub" />
+          </div>
+        </div>
+      </vuescroll>
     </v-list>
   </v-menu>
 </template>
 
 <script>
+import vuescroll from 'vuescroll'
 import RecursiveMenu from '@/components/elements/RecursiveMenu.vue'
 
 export default {
@@ -34,7 +39,7 @@ export default {
   props: {
     item: Object,
   },
-  components: { RecursiveMenu },
+  components: { RecursiveMenu, vuescroll },
   methods: {
     close(originalFunction) {
       originalFunction()
