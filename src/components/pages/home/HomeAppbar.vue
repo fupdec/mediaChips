d2ff826ed31c0652bcef940d9c915a8c93b58b7f
fupdec <>
2021-09-17 18:22:02 +0300

    a. editing state of widgets

--- src/components/pages/home/HomeAppbar.vue
+++ src/components/pages/home/HomeAppbar.vue
@@ -12,51 +12,6 @@
     </div>
     
     <v-spacer></v-spacer>
-    
-    <v-menu offset-y nudge-bottom="10" :close-on-content-click="false">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn v-bind="attrs"  v-on="on" icon tile>
-          <v-icon>mdi-counter</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-btn-toggle v-model="$store.state.quantityRecentVideos"
-          @change="changeQuantityRecentVideos" :value="$store.state.quantityRecentVideos"
-          color="primary" mandatory
-        > 
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" outlined :value="10">10</v-btn>
-            </template>
-            <span>10 previews</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" outlined :value="20">20</v-btn>
-            </template>
-            <span>20 previews</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" outlined :value="30">30</v-btn>
-            </template>
-            <span>30 previews</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" outlined :value="50">50</v-btn>
-            </template>
-            <span>50 previews</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" outlined :value="80">80</v-btn>
-            </template>
-            <span>80 previews</span>
-          </v-tooltip>
-        </v-btn-toggle>
-      </v-card>
-    </v-menu>
   </div>
 </template>
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -23,8 +23,23 @@
       </v-row>
 
       <v-row v-if="videosNumber>0">
+        <v-col cols="12" class="py-0 text-right">
+          <v-btn @click="customization=!customization" class="mb-2" rounded color="primary"> 
+            <v-icon left>mdi-cog</v-icon> {{customization?'Finish Customization':'Customize widgets'}} </v-btn>
+        </v-col>
+
+        <v-col v-if="customization" cols="12">
+          <v-card outlined>
+            <v-card-actions>
+              <div>Graph with number of videos added and edited per days</div>
+              <v-spacer></v-spacer>
+              <v-switch v-model="widgets.graphVideos" @change="updateWidget($event, 'graphVideos')" inset hide-details class="ma-2 pt-0"
+                :append-icon="`mdi-${widgets.graphVideos?'eye':'eye-off'}`"/>
+            </v-card-actions>
+          </v-card>
+        </v-col>
         <v-col cols="12">
-          <v-card>
+          <v-card v-if="customization||widgets.graphVideos">
             <v-toolbar color="secondary">
               <div class="headline">Number of videos added and edited per days</div>
               <v-spacer></v-spacer>
@@ -36,8 +51,18 @@
           </v-card>   
         </v-col>
       
+        <v-col v-if="customization" cols="12">
+          <v-card outlined>
+            <v-card-actions>
+              <div>Total values of all videos</div>
+              <v-spacer></v-spacer>
+              <v-switch v-model="widgets.numberVideos" @change="updateWidget($event, 'graphVideos')" inset hide-details class="ma-2 pt-0"
+                :append-icon="`mdi-${widgets.numberVideos?'eye':'eye-off'}`"/>
+            </v-card-actions>
+          </v-card>
+        </v-col>
         <v-col cols="12">
-          <div class="d-flex flex-wrap justify-space-around">
+          <div v-if="customization||widgets.numberVideos" class="d-flex flex-wrap justify-space-around">
             <v-card outlined class="pa-2">
               <v-icon>mdi-database</v-icon> Total Number of Videos:
               <b v-text="$store.getters.videosTotal"/>
@@ -53,12 +78,23 @@
           </div>
         </v-col>
 
-        <v-col cols="12">
+        <v-col v-if="customization" cols="12">
           <v-card outlined>
-            <v-toolbar color="secondary" height="40">
+            <v-card-actions>
+              <div>Recently added videos</div>
               <v-spacer></v-spacer>
-              <div class="headline">{{$store.state.quantityRecentVideos}} recently added videos</div>
+              <v-switch v-model="widgets.recentVideos" @change="updateWidget($event, 'graphVideos')" inset hide-details class="ma-2 pt-0"
+                :append-icon="`mdi-${widgets.recentVideos?'eye':'eye-off'}`"/>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card v-if="customization||widgets.recentVideos" outlined>
+            <v-toolbar color="secondary">
+              <div class="headline">{{numberRecentVideos}} recently added videos</div>
               <v-spacer></v-spacer>
+              <v-btn to="/videos/:default?tabId=default" draggable="false" outlined>
+                <v-icon left>mdi-video</v-icon> Show All Videos</v-btn>
             </v-toolbar>
             <div class="previews-grid" @mousedown="stopSmoothScroll($event)"> 
               <v-hover v-for="video in recentVideos" :key="video.id">
@@ -75,23 +111,26 @@
                 </template>
               </v-hover>
             </div>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="pr-4" color="secondary" to="/videos/:default?tabId=default" draggable="false" rounded>
-                <v-icon left>mdi-video</v-icon> Show All Videos</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
           </v-card>
         </v-col>
 
-        <v-col cols="12" v-for="m in complexMetaAssignedToVideo" :key="m.id">
+        <v-col v-if="customization" cols="12">
           <v-card outlined>
-            <v-toolbar color="secondary" height="40">
+            <v-card-actions>
+              <div>Top 10 most viewed meta cards</div>
               <v-spacer></v-spacer>
-              <div class="headline">
-                Top 10 Most Viewed {{getMeta(m.id).settings.name}}
-              </div>
+              <v-switch v-model="widgets.topViewedMetaCards" @change="updateWidget($event, 'graphVideos')" inset hide-details class="ma-2 pt-0"
+                :append-icon="`mdi-${widgets.topViewedMetaCards?'eye':'eye-off'}`"/>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col v-if="customization||widgets.topViewedMetaCards" cols="12">
+          <v-card v-for="m in complexMetaAssignedToVideo" :key="m.id" outlined class="mb-6">
+            <v-toolbar color="secondary">
+              <div class="headline"> Top 10 Most Viewed {{getMeta(m.id).settings.name}} </div>
               <v-spacer></v-spacer>
+              <v-btn :to="`/meta/?metaId=${m.id}&tabId=default`" draggable="false" outlined>
+                <v-icon left>mdi-{{getMeta(m.id).settings.icon}}</v-icon> Show All {{getMeta(m.id).settings.name}}</v-btn>
             </v-toolbar>
             <div class="previews-grid">
               <v-card v-for="mc in getTopMetaCards(m.id)" :key="mc.id" outlined hover
@@ -106,18 +145,12 @@
                 <div class="caption px-1">{{mc.meta.name}} <b>{{mc.views||0}}</b></div>
               </v-card>
             </div>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="pr-4" color="secondary" to="/videos/:default?tabId=default" draggable="false" rounded>
-                <v-icon left>mdi-{{getMeta(m.id).settings.icon}}</v-icon> Show All {{getMeta(m.id).settings.name}}</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
           </v-card>
         </v-col>
 
         <v-col cols="12">
-          <v-btn class="mb-4" x-large block rounded color="secondary" to="/settings" draggable="false"> 
-            <v-icon left>mdi-cog</v-icon> Open settings</v-btn>
+          <v-btn @click="customization=!customization" class="mb-4" x-large block rounded color="primary"> 
+            <v-icon left>mdi-cog</v-icon> {{customization?'Finish Customization':'Customize widgets'}} </v-btn>
         </v-col>
       </v-row>
 
@@ -153,6 +186,7 @@ export default {
   },
   mixins: [LabelFunctions, MetaGetters], 
   beforeMount() {
+    this.initWidgets()
     this.initVideosStat(7)
   },
   mounted() {
@@ -164,24 +198,29 @@ export default {
     createAllMeta: false,
     isAllMetaCreated: false,
     // stats
+    numberRecentVideos: 20,
+    customization: false,
     series: [],
     chartOptions: {},
   }),
   computed: {
     settings() { return this.$store.getters.settings.value() },
-    previewsNumber() { return this.$store.state.quantityRecentVideos },
-    recentVideos() { return this.$store.getters.videos.orderBy('date', ['desc']).take(this.previewsNumber).value() },
+    recentVideos() { return this.$store.getters.videos.orderBy('date', ['desc']).take(this.numberRecentVideos).value() },
     pathToUserData() { return this.$store.getters.getPathToUserData },
     logoPath() { return path.join('file://', __static, '/icons/icon.png') },
     videosNumber() { return this.$store.getters.videos.value().length },
     metaNumber() { return this.$store.getters.meta.filter(i=>i.type!=='specific').value().length },
     complexMetaAssignedToVideo() { return this.$store.getters.settings.get('metaAssignedToVideos').filter({type:'complex'}).value() },
     daysBefore() { 
-      let v = this.$store.getters.videos.orderBy(i=>i.date, ['asc']).value().splice(0,1)
+      let v = this.$store.getters.videos.orderBy(i=>i.date, ['asc']).take(1).value()
       if (v.length) return Math.floor((Date.now() - v[0].date)/(60*60*1000*24))
       else return 7
     },
     darkMode() { return this.$store.state.Settings.darkMode },
+    widgets: {
+      get() { return this.$store.state.Settings.widgets },
+      set(value) { this.$store.dispatch('updateSettingsState', {key:'widgets', value}) },
+    },
   },
   methods: {
     initVideosStat(days) {
@@ -210,12 +249,22 @@ export default {
         xaxis: { type: 'datetime', categories: dates },
       }
     },
+    initWidgets() {
+      let defaultWidgets = {
+        graphVideos: true,
+        numberVideos: true,
+        recentVideos: true,
+        topViewedMetaCards: true,
+      }
+      this.widgets = {...defaultWidgets, ...this.widgets}
+    },
+    updateWidget(value, widgetName) { this.widgets = {...this.widgets, ...{[widgetName]: value}} },
     stopSmoothScroll(event) {
       if(event.button != 1) return
       event.preventDefault()
       event.stopPropagation()
     },
-    getTopMetaCards(metaId) { return this.$store.getters.metaCards.filter({metaId}).orderBy(i=>i.views||0, ['desc']).value().slice(0,10) },
+    getTopMetaCards(metaId) { return this.$store.getters.metaCards.filter({metaId}).orderBy(i=>i.views||0, ['desc']).take(10).value() },
     getVideoThumbUrl(videoId) {
       let imgPath = path.join(this.pathToUserData, `/media/thumbs/${videoId}.jpg`)
       return 'file://' + this.checkVideoImageExist(imgPath)
@@ -272,7 +321,7 @@ export default {
     openLink(link) { shell.openExternal(link) },
   },
   watch: {
-    darkMode() { setTimeout(() => { this.initVideosStat(7) }, 1000) },
+    darkMode() { setTimeout(() => { this.initVideosStat(7) }, 3000) },
   }
 }
 </script>
